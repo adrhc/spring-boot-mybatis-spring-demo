b8b6dac9c14a3e0a600b3bb0b9bf7114e7518458
Adrian Petre <adrian.petre@endava.com>
2022-06-14 12:37:49 +0300

    application.yml

--- src/main/java/ro/go/adrhc/springbootmybatisspringdemo/SpringBootMybatisSpringDemoApplication.java
+++ src/main/java/ro/go/adrhc/springbootmybatisspringdemo/SpringBootMybatisSpringDemoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootMybatisSpringDemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMybatisSpringDemoApplication.class, args);
 	}
-
 }
