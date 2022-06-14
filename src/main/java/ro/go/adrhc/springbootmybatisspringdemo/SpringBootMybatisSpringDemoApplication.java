73905dd0bb9e72b5e66b58217aa885ec6fc1bbb4
Adrian Petre <adrian.petre@endava.com>
2022-06-14 12:37:05 +0300

    Initial commit

---
+++ src/main/java/ro/go/adrhc/springbootmybatisspringdemo/SpringBootMybatisSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package ro.go.adrhc.springbootmybatisspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMybatisSpringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/go/adrhc/springbootmybatisspringdemo/SpringBootMybatisSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.go.adrhc.springbootmybatisspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMybatisSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
